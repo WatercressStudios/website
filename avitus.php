d9bd1e28a22b4de6d13bac2704117977d3ebe501
Lixian <weiyuanleeif@gmail.com>
2021-08-04 16:57:37 -0400

    css grid added to both about page and avitus page. cards on about are mostly responsive. avitus is somewhat responsive

--- avitus.php
+++ avitus.php
@@ -5,82 +5,90 @@
     <?php include 'head.php'; ?>
     <?php include 'header.php'; ?>
     <link rel="stylesheet" href="avitus-style.css">
-    <div class="game-description">
-    <div class="avitus-slideshow">
-        <img src="https://via.placeholder.com/900x350">
-    </div>
-    <div class="project-description">
-        <h1 class="avitus-header">Premise</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod malesuada bibendum. Duis non dignissim
-            risus. In hac habitasse platea dictumst. Nam fringilla neque pretium risus accumsan interdum. Quisque
-            laoreet vitae est et pulvinar. Proin ultricies vestibulum libero, et fringilla mauris laoreet et. Sed
-            commodo finibus enim vitae egestas. Nunc eleifend porttitor tellus eget condimentum. Morbi neque enim,
-            interdum eu ultricies at, volutpat a enim. Sed ut consequat sapien. Phasellus a erat quis diam tristique
-            pellentesque. Proin a cursus sem. Morbi placerat faucibus massa sit amet luctus. Donec eget convallis elit.
-            Pellentesque pellentesque dui a enim accumsan, et euismod metus varius. Mauris ullamcorper lobortis libero
-            sit amet sollicitudin.</p>
-    </div>
-    <div class="project-description">
-        <h1 class="avitus-header">Features</h1>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Morbi venenatis sapien id condimentum laoreet.
-            Morbi eget felis posuere, rhoncus neque in, iaculis ante.
-            Phasellus lobortis ipsum ac condimentum pellentesque.
-            Sed commodo dolor in massa bibendum varius.
-            Pellentesque vel nunc quis velit volutpat fermentum sit amet ut ante.
-            Mauris rhoncus urna id viverra varius.
-            Proin lobortis est at nisi facilisis posuere.
-            Praesent venenatis lorem at turpis aliquam rutrum.
-            Fusce pharetra nulla a quam dapibus aliquam.
-            Sed euismod nulla ac metus accumsan, accumsan ornare lorem porttitor.
-            Nulla vehicula turpis at euismod facilisis.</p>
-    </div>
-    <div class="trailor-media">
-        <h1 class="avitus-header">Trailer</h1>
-    </div>
-
-    <div class="cast-section">
-        <h1 class="avitus-header">Cast</h1>
-        <div class="container">
-            <div class="character-descriptions">
-                <img src="https://via.placeholder.com/350">
-                <h1 class="avitus-header">Character</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada blandit arcu ac vehicula.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
-                    blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus fringilla.
-                </p>
-            </div>
-            <div class="character-descriptions">
-                <img src="https://via.placeholder.com/350">
-                <h1 class="avitus-header">Character</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada blandit arcu ac vehicula.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
-                    blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus fringilla.
-                </p>
-            </div>
-            <div class="character-descriptions">
-                <img src="https://via.placeholder.com/350">
-                <h1 class="avitus-header">Character</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada blandit arcu ac vehicula.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
-                    blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus fringilla.
-                </p>
-            </div>
-            <div class="character-descriptions">
-                <img src="https://via.placeholder.com/350">
-                <h1 class="avitus-header">Character</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada blandit arcu ac vehicula.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
-                    blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus fringilla.
-                </p>
-            </div>
-
+   
+        <div class="project-description">
+        <div class="avitus-slideshow">
+            <img src="https://via.placeholder.com/900x250">
         </div>
-    </div>
+            <h1 class="avitus-header">Premise</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod malesuada bibendum. Duis non
+                dignissim
+                risus. In hac habitasse platea dictumst. Nam fringilla neque pretium risus accumsan interdum. Quisque
+                laoreet vitae est et pulvinar. Proin ultricies vestibulum libero, et fringilla mauris laoreet et. Sed
+                commodo finibus enim vitae egestas. Nunc eleifend porttitor tellus eget condimentum. Morbi neque enim,
+                interdum eu ultricies at, volutpat a enim. Sed ut consequat sapien. Phasellus a erat quis diam tristique
+                pellentesque. Proin a cursus sem. Morbi placerat faucibus massa sit amet luctus. Donec eget convallis
+                elit.
+                Pellentesque pellentesque dui a enim accumsan, et euismod metus varius. Mauris ullamcorper lobortis
+                libero
+                sit amet sollicitudin.</p>
+        </div>
+        <div class="project-description">
+            <h1 class="avitus-header">Features</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Morbi venenatis sapien id condimentum laoreet.
+                Morbi eget felis posuere, rhoncus neque in, iaculis ante.
+                Phasellus lobortis ipsum ac condimentum pellentesque.
+                Sed commodo dolor in massa bibendum varius.
+                Pellentesque vel nunc quis velit volutpat fermentum sit amet ut ante.
+                Mauris rhoncus urna id viverra varius.
+                Proin lobortis est at nisi facilisis posuere.
+                Praesent venenatis lorem at turpis aliquam rutrum.
+                Fusce pharetra nulla a quam dapibus aliquam.
+                Sed euismod nulla ac metus accumsan, accumsan ornare lorem porttitor.
+                Nulla vehicula turpis at euismod facilisis.</p>
+        </div>
+        <div class="project-description">
+        <div class="trailor-media">
+            <h1 class="avitus-header">Trailer</h1>
+        </div>
 </div>
 
     
+        <div class="container">
+            <div class="character-cast">
+            <h1 class="avitus-header">Cast</h1>
+                <div class="character-card">
+                    <img class="character-img" src="https://via.placeholder.com/250">
+                    <h1 class="character-header">Character</h1>
+                    <p class="character-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                        malesuada blandit arcu ac
+                        vehicula.
+                        Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
+                        blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus
+                        fringilla.
+                    </p>
+                </div>
+                <div class="character-card">
+                    <img class="character-img" src="https://via.placeholder.com/250">
+                    <h1 class="character-header">Character</h1>
+                    <p class="character-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                        malesuada blandit arcu ac
+                        vehicula.
+                        Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
+                        blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus
+                        fringilla.
+                    </p>
+                </div>
+                <div class="character-card">
+                    <img class="character-img" src="https://via.placeholder.com/250">
+                    <h1 class="character-header">Character</h1>
+                    <p class="character-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                        malesuada blandit arcu ac
+                        vehicula.
+                        Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat, elit sed placerat
+                        blandit, lorem dolor tempus turpis, quis fermentum ex diam id urna. Duis pharetra luctus
+                        fringilla.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+<div class="project-description">
+
     <div class="content-warning">
         <h1 class="avitus-header">Content Warning</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada blandit arcu ac
@@ -92,6 +100,7 @@
     <div class="download-section">
         <button>Download on Steam</button> <button>Download on Itch.io</button>
     </div>
+</div>
 </body>
 
 </html>
