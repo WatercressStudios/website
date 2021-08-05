64de8268cfb32232c77edb63a5a4a4e1401cfe63
Happiness+ <lovevns@protonmail.com>
2021-08-05 18:50:40 -0400

    cleaner
    it's cleaner now

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <?php include 'head.php'; ?>
 <body>
 <!-- Dear Happiness+ in the future, set up CI/CD FTP with our github -->
-<div class="container">
+<div class="container" style="padding: 5%">
 
 
 	<h1>Hey everyone, we are currently making remaking this whole website for the time being. </h1>
     <p>For now here is a picture of Budapast to keep you entertained</p>
-    <img height="500" width="500" alt="Budapest" loading="lazy" src="budascene6.webp">
+    <img height="50%" width="100%" alt="Budapest" loading="lazy" src="budascene6.webp">
 	<noscript>Your browser does not support JavaScript! Why?!</noscript>
 	<?php include 'footer.php'; ?>
 </div>
